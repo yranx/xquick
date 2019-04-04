ff39e67c1bf4838003946941b0c3d1b35878de98
ranx <yranforever@163.com>
2019-04-04 09:16:10 +0800

    Prototype pattern

---
+++ chowder/src/main/java/com/ranx/chowder/study/designPatterns/prototype_copyclone/ShallowThing.java
@@ -0,0 +1,35 @@
+package com.ranx.chowder.study.designPatterns.prototype_copyclone;
+
+import java.util.ArrayList;
+
+/**
+ * 浅拷贝
+ * @author ranx
+ * @create 2019-03-26 14:39
+ **/
+public class ShallowThing implements Cloneable{
+    //定义一个私有变量
+    private  ArrayList<String> arrayList = new ArrayList<String>();
+
+    @Override
+    public ShallowThing clone(){
+        ShallowThing shallowThing = new ShallowThing();
+        try {
+            shallowThing = (ShallowThing) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return shallowThing;
+    }
+
+    //设置HashMap的值
+    public void setValue(String value) {
+        this.arrayList.add(value);
+    }
+
+    //取得arrayList的值
+    public ArrayList<String> getValue() {
+        return this.arrayList;
+    }
+}
