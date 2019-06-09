a878c7a8794f70cc5ae23eb132bf90075a1646ea
ranx <yranforever@163.com>
2019-06-09 20:46:25 +0800

    review: 自定义注解

---
+++ chowder/src/main/java/com/ranx/chowder/study/cusannotation/AnnoOut.java
@@ -0,0 +1,12 @@
+package com.ranx.chowder.study.cusannotation;
+
+/**
+ * @author ranx
+ * @create 2019-06-09 19:59
+ **/
+public class AnnoOut {
+    @CusAnnotation(hello = "Hello, ShangHai", world = "Hello, World")
+    public void output() {
+        System.out.println("method output id running");
+    }
+}
---
+++ chowder/src/main/java/com/ranx/chowder/study/cusannotation/CusAnnotation.java
@@ -0,0 +1,13 @@
+package com.ranx.chowder.study.cusannotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CusAnnotation {
+    String hello() default "hello";
+    String world();
+}
